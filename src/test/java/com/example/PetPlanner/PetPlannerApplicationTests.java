29dda4447210e71927adf2c93afab809f1793263
unknown <kalinic.ra71.2019@uns.ac.rs>
2023-04-08 22:08:33 +0200

    setup project

---
+++ src/test/java/com/example/PetPlanner/PetPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
